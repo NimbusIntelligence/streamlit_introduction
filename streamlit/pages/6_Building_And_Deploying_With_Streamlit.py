2e51e4adb4a21119034513cd59717f4b61ca1246
Sebastian Wiesner <38764832+wiesnseb@users.noreply.github.com>
2024-01-12 13:49:08 +0100

    after presentation

--- streamlit/pages/6_Building_And_Deploying_With_Streamlit.py
+++ streamlit/pages/6_Building_And_Deploying_With_Streamlit.py
@@ -4,7 +4,7 @@ st.progress(66, text="Presentation Progress")
 
 st.header("Building And Deploying With Streamlit")
 
-uploaded_file = st.file_uploader("Upload a Picture", type="png")
+uploaded_file = st.file_uploader("Import image '6_Building_And_Deploying_With_Streamlit' from the repo here", type="png")
 
 if uploaded_file is not None:
     st.write("Here is the Picture you uploaded:")
